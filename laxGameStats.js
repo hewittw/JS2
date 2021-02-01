1275741caada4d461373fb6931c9dfbc66553a36
Hewitt School Account <School@Hewitts-MBP.bishops.com>
2021-02-01 09:26:32 -0800

    Class work. Worked on the data portion. Now need to add code that does stuff with data. Also need to add in api.

--- laxGameStats.js
+++ laxGameStats.js
@@ -1,21 +1,113 @@
-// to do's
-// fix the vehicles one with the json
-// fix the adding json's to a list of json's for storage
-// get dr. j to look at html code
-// ask if you have to do the api - might be too hard
-// how use a good password thing
-function findRadioBox (genders) {
+
+// Google Sheets API Stuff
+
+// Searching and displaying data
+
+//***************************************************************************//
+//***************************************************************************//
+//***************************************************************************//
+
+// functions that use the data and interact with user
+
+function displayOptions(){
   /*
-  Purpose: Find the radio box selected
-  Parameters: list of the radio box element id info stored in a variable
-  Returns: the value of the clicked radio box element
+  Purpose: add html code to page that lets the user search through their data
+  Parameters: None
+  Returns: None
   */
-  for (i = 0; i < 3; i++) {
-    if (genders[i].checked) {
-      return genders[i].value;
-    }
+}
+
+//***************************************************************************//
+//***************************************************************************//
+//***************************************************************************//
+
+// fucntions to do with getting, storing, and organizing Data
+
+function getData(){
+  /*
+  Purpose: Get all the data from the current submission and store into a JSON
+  Parameters: None
+  Returns: A JSON of all the data
+  */
+  var fname = document.getElementById('fname').value; // can we just throw these into the json??????
+  var playerNum = document.getElementById('playerNum').value;
+  var numGoals = document.getElementById('numGoals').value;
+
+  // Get the value of the radio boxes.
+  var defense = document.getElementById('defense');
+  var midfield = document.getElementById('midfield');
+  var attack = document.getElementById('attack');
+  var positions = [defense, midfield, attack];
+
+  // // Get the value of the check boxes.
+  // Note: don't use but getting them just in case I want do something with them later after we learn more code
+  var btb = document.getElementById('btb');
+  var icePick = document.getElementById('icePick');
+  var atw = document.getElementById('atw');   // is there a better way to do this that does not need the list?????????
+  var bigPlays = [btb, icePick, atw];
+
+  // ADD OTHER INPUTS HERE !!!!!!!!!!!!!!!!!!!!
+
+  // Talk to dr. j about the best way to store this data!!!!!
+  gameStats = {
+    "fname": document.getElementById('fname').value,
+    "playerNum": document.getElementById('playerNum').value,
+    "numGoals": document.getElementById('numGoals').value,
+    "defense": defense,
+    "midfield": midfield,
+    "attack": attack,
+    "positions": positions,
+    "btb": btb,
+    "icePick": icePick,
+    "atw": atw,
+    "bigPlays": bigPlays,
+  }
+  return gameStats;
+
+}
+
+function writeData(gameStats, games){
+  /*
+  Purpose: Add the current JSON data to the list of JSONs
+  Parameters: the new JSON (type: JSON) and the list of JSONs (type: array)
+  Returns: The updaded game list of JSONs
+  */
+  if (games != null){
+    games.push(gameStats);
+
+    stringGames = JSON.stringify(games);
+    localStorage.setItem("games", stringGames);
+
+    return games;
+
+  } else {
+    var games = [gameStats];
+
+    stringGames = JSON.stringify(games);
+    localStorage.setItem("games", stringGames);
+
+    return games;
   }
 }
+
+function readData(){
+  /*
+  Purpose: Get the list of JSONs from local storage and return it
+  Parameters: None
+  Returns: A list of JSONs
+  */
+  stringGames = localStorage.getItem("games");
+  games = JSON.parse(stringGames);
+  if (games != null){
+    return games;
+  } else {
+    return null;
+  }
+}
+
+//***************************************************************************//
+//***************************************************************************//
+//***************************************************************************//
 
 // Click event to attach to button
 function myClick () {
@@ -25,114 +117,30 @@ function myClick () {
   Parameters: None
   Returns: None
   */
-  // Quick check to verify that the function executes.
-  console.log("test function");
-  // Get the values that were input into the two text boxes.
-  var fname = document.getElementById('fname').value;
-  var lname = document.getElementById('lname').value;
-  var favColor = document.getElementById('favColor').value;
 
-  // Get the value of the radio boxes.
-  var male = document.getElementById('male');
-  var female = document.getElementById('female');
-  var other = document.getElementById('other');
-  var genders = [male, female, other];
-
-  // // Get the value of the check boxes.
-  // Note: don't use but getting them just in case I want do something with them later after we learn more code
-  var bike = document.getElementById('Bike');
-  var car = document.getElementById('Car');
-  var boat = document.getElementById('Boat');
-  var vehicles = [bike, car, boat];
-
-  // Other inputs test JS
-  var myURL = document.getElementById("myURL") // ask dr. J why this isn't working!!!!!!!!!
-  //window.open(myURL);  // get this working later with an iframe - couldn't get to work right now
-
-  // JSON test code - not fully integrated yet, just trying it out
-  myJSON = {
-    "fname": fname,
-    "lname": lname,
-    "favColor": favColor,
-    "male": male,
-    "female": female,
-    "other": other,
-    "genders": genders,
-    "bike": bike,
-    "car": car,
-    "boat": boat,
-    "vehicles": vehicles,
-    "myURL": myURL,
-  }
-  console.log(myJSON);
-
-  console.log(JSON.stringify(myJSON));
-  console.log(JSON.parse(JSON.stringify(myJSON)));
+  // Reading, writing, and updating the data
+  games = readData(); // variable of old info - null if no previous data
+  gameStats = getData(); // the current data stored in a JSON
+  games = writeData(gameStats, games); // updates the Games list and returns the updated list
 
 
-  // Display values
+  // some test code
+  console.log(gameStats);
+  console.log(JSON.stringify(gameStats));
+  console.log(JSON.parse(JSON.stringify(gameStats)));
+  console.log(games);
 
-  // Display text box values
-  if (myJSON['fname'] != "" & myJSON['lname'] != "") {
-    myDiv.innerHTML = "Your name is " + myJSON['fname'] + " " + myJSON['lname'] + ". ";
-  } else {
-    myDiv.innerHTML = "Please enter your names again. We did not get all the data we needed.";
-  }
+  clicked = true; // now the other part of the program can be show / diplayed to user bc info was submitted
+  displayOptions();
 
-  if (myJSON['favColor'] != "") {
-    myDiv.innerHTML += "<br>Your favorite color is " + myJSON['favColor'] + ". ";
-  } else {
-    myDiv.innerHTML += "<br>Please enter your favorite color.";
-  }
-
-
-  // HERE AND BELOW NEED TO ADD THE JSON STUFF - JUST WANT TO TRY IT OUT ABOVE MORE
-
-
-  // Display check boxes values
-  var numVehicles = 0;
-  for (i = 0; i < 3; i++) {
-    if (myJSON['vehicles'][i].checked) {
-      numVehicles++;
-    }
-  }
-  myDiv.innerHTML += "<br>You had " + numVehicles + " out of 3 vehicles.";
-
-  // Display Radio box values
-  userGender = findRadioBox(genders);
-  myDiv.innerHTML += "<br>You identify as " + userGender + ". ";
-
-
-  // Check local storage and display data
-  var oldLastName = localStorage.getItem("lastName");
-  console.log(oldLastName);
-  if (oldLastName != null ) {
-    myDiv.innerHTML += "<br>Last time you answered your last name was " + oldLastName + ". ";
-  }
-
-  // Add last name to local storage
-  if (lname != "") {
-    localStorage.setItem("lastName", lname);
-  }
-  //***************************************************************************//
-  //***************************************************************************//
-  //***************************************************************************//
-  oldJSON = localStorage.getItem("oldJSON");
-  if (oldJSON != null ) {
-    console.log(oldJSON);
-    oldData = JSON.parse(oldJSON);
-    if (oldData['fname'] != ""){
-      myDiv.innerHTML += "<br>Last time you answered your first name was " + oldData['fname'];
-    }
-  }
-
- // use an array here
-  // Add this JSON to local storage
-  stringJSON = JSON.stringify(myJSON)
-  localStorage.setItem("oldJSON", stringJSON);
 }
 
-// Main Section
-// Just check to see that we found the div element we are looking for
+//***************************************************************************//
+//***************************************************************************//
+//***************************************************************************//
+
+
+// Mainf (Global variables also here becasue not in a function)
 myDiv = document.getElementById("myDiv");
 console.log(myDiv);
+var clicked = false;
